2efd18d8f689e0480ccf40ba45163b4fb9effa3a
eryanaekarizqia-cloud <eryanaekarizqia@gmail.com>
2026-09-15 22:58:27 +0700

    refactor: import assets via ESM and add Vite client types

--- src/assets/proseduriaAssets.ts
+++ src/assets/proseduriaAssets.ts
@@ -3,11 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import bgMain from './images/proseduria_bg_main_1789447636784.jpg';
+import aksaraMascot from './images/aksara_mascot_smp_1789447661733.jpg';
+import worldGlitch from './images/world_glitch_state_1789447684307.jpg';
+import worldRestored from './images/world_restored_state_1789447703547.jpg';
+import badgeTrophy from './images/badge_trophy_set_1789447722269.jpg';
+import nusantaraWorldMap from './images/nusantara_world_map_1789448694165.jpg';
+
 export const PROSEDURIA_ASSETS = {
-  bgMain: '/src/assets/images/proseduria_bg_main_1789447636784.jpg',
-  aksaraMascot: '/src/assets/images/aksara_mascot_smp_1789447661733.jpg',
-  worldGlitch: '/src/assets/images/world_glitch_state_1789447684307.jpg',
-  worldRestored: '/src/assets/images/world_restored_state_1789447703547.jpg',
-  badgeTrophy: '/src/assets/images/badge_trophy_set_1789447722269.jpg',
-  nusantaraWorldMap: '/src/assets/images/nusantara_world_map_1789448694165.jpg',
+  bgMain,
+  aksaraMascot,
+  worldGlitch,
+  worldRestored,
+  badgeTrophy,
+  nusantaraWorldMap,
 };
